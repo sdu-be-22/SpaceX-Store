e401e1c0b5dd17c6fc4b6f993cc8f9e9a252db46
diidara <92643449+diidara@users.noreply.github.com>
2022-04-23 03:07:07 +0600

    Update views.py

--- bookshop/base/views.py
+++ bookshop/base/views.py
@@ -1,1 +1,9 @@
-
+from django.shortcuts import redirect, render, get_object_or_404, reverse
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView,
+	UpdateView,
+	DeleteView,
+	FormView
+)
