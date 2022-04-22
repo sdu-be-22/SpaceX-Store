497cc5bbf48183d941b8b9778c290eb21d2f542c
Yermukhambetova Aidana <103299802+Aidanaa22@users.noreply.github.com>
2022-04-23 03:13:26 +0600

    Update views.py

--- bookshop/base/views.py
+++ bookshop/base/views.py
@@ -32,7 +32,7 @@ from django.db.models import Sum
 
 		class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-
+         
     class Meta:
         model = User
         fields = ['username', 'email' , 'first_name','last_name' ]
@@ -42,3 +42,17 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'base/signup.html'
+class BooksListView(ListView):
+	model = Book
+	template_name = 'base/list.html' 
+	ordering = ['-pk']
+	
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['filter'] = BookFilter(self.request.GET, queryset = self.get_queryset())
+		paginated_filtered_jobs = Paginator(context['filter'].qs, 32)
+		page_number = self.request.GET.get('page')
+		job_page_obj = paginated_filtered_jobs.get_page(page_number)
+		context['book_page_obj'] = job_page_obj
+		return context
