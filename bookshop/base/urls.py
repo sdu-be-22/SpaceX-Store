c9178dab2b520ba4111530eeb2cbfffdacdc0174
kamilamukhamadiyeva <73333360+kamilamukhamadiyeva@users.noreply.github.com>
2022-04-23 02:55:15 +0600

    Update urls.py

--- bookshop/base/urls.py
+++ bookshop/base/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('profile', profile, name='profile'),
-
-  path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
+    path('', (BooksListView.as_view()), name='book-list'),
+    path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='base/logout.html'), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
