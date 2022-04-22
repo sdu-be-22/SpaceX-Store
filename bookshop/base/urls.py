e67cd23b1f8770011ea6fd24fd64cf1494888600
diidara <92643449+diidara@users.noreply.github.com>
2022-04-23 02:51:55 +0600

    Update urls.py

--- bookshop/base/urls.py
+++ bookshop/base/urls.py
@@ -1,1 +1,5 @@
-
+from django.urls import path, include
+from .views import (BooksListView, BookDetailView, SignUpView, profile,
+    all_cart, add_to_cart, delete_from_cart, delete_cart, delete_all_cart,
+    orderCreate, OrderUpdate)
+from django.contrib.auth import views as auth_views
