5bc331f4517a8b0b37a6bcf68fa9d92616d8a28f
Yermukhambetova Aidana <103299802+Aidanaa22@users.noreply.github.com>
2022-04-23 02:43:50 +0600

    Add files via upload

---
+++ bookshop/base/filters.py
@@ -0,0 +1,11 @@
+import django_filters
+from .models import Book
+from django import forms
+
+class BookFilter(django_filters.FilterSet):
+	name=django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={
+            'placeholder': 'поиск...', 'class': 'form-control mb-2 mr-sm-2'})) 
+	class Meta:
+		model = Book
+		fields = ['name', 'genre']
+
