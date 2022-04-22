1fcd7448a19589173478c32075630560ebe15c98
diidara <92643449+diidara@users.noreply.github.com>
2022-04-23 02:59:01 +0600

    Update models.py

--- bookshop/base/models.py
+++ bookshop/base/models.py
@@ -1,1 +1,23 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 
+
+class Genre(models.Model):
+	name = models.CharField(max_length=64)
+
+	def __str__(self):
+		return self.name 
+
+# Create your models here.
+class Book(models.Model):
+	name = models.CharField(max_length=128)
+	image = models.ImageField(upload_to='images/', blank=True)
+
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+	price = models.IntegerField(default=0)
+	about = RichTextField()
+
+	def __str__(self):
+		return self.name
